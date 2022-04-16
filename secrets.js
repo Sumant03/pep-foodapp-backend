e4fbee8233710b3074b25f85c6eb99a73da05928
Sumant <sumant.sharma.nonu@gmail.com>
2022-04-16 16:34:53 +0530

    added

--- secrets.js
+++ secrets.js
@@ -1,6 +1,6 @@
 module.exports = {
     key: "abcd",
-    PASSWORD:"QSfTt1LsNTCRNX90",
+    PASSWORD:"nB6mIri0o1KAWgEz",
     APP_PASSWORD:"ipichqbfcwzvmtpe",
     KEY_ID:"rzp_test_zdp4Mww66oBTqC",
     KEY_SECRET:"8fndv80K6Qsh8dLsBWbgoA35"
